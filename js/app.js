1288617439a690680e8224b57e5abb1b90b4546c
magentaFire <dafne.magenta@gmail.com>
2017-05-03 16:10:43 -0500

    terminada estructura de applove II

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+var images = document.getElementsByClassName("workImage");
+var btnCerrar = document.getElementsByClassName("close");
+
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", modal);
+}
+
+for (var i = 0; i < btnCerrar.length; i++) {
+  btnCerrar[i].addEventListener("click", cerrarModal);
+}
+
+function modal() {
+  this.style.display = "block";
+  console.log("holi");
+}
+
+function cerrarModal() {
+  console.log("adio");
+}
